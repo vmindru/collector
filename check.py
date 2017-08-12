63803a0d3fb53f095555c5f5451b107e33c49322
Veaceslav Mindru <mindruv@gmail.com>
2017-08-13 01:25:19 +0200

    WIP: all implemented except send

--- check.py
+++ check.py
@@ -4,6 +4,7 @@ import commands
 from argparse import ArgumentParser
 from time import time
 import platform
+# import socket
 
 
 VERSION = "0.1"
@@ -13,15 +14,17 @@ hostname = str(platform.node())
 def collect_arguments():
     parser = ArgumentParser()
     parser.prog = "collector"
-    parser.description = 'collector: collects and sends stats to collectd'
+    parser.description = 'collector: collects and sends stats to carbon'
     group = parser.add_mutually_exclusive_group(required=True)
     parser.add_argument('-v', '--version', action='version', version=VERSION, help='display version information')
     parser.add_argument('-o', '--oid', help='OID where to store data')
     parser.add_argument('-c', '--command', required=True, help='command to execute', metavar='')
-    parser.add_argument('-V', '--verbose', help='print data to stdout before sending to server')
-    group.add_argument('-t', '--time', action='store_true', help='send command time execution as metric to collectd')
-    group.add_argument('-d', '--data', action='store_true', help='send command data to collectd, must be integer or '
+    parser.add_argument('-V', '--verbose', action='store_true', help='print data to stdout before sending to server')
+    group.add_argument('-t', '--time', action='store_true', help='send command time execution as metric to carbon')
+    group.add_argument('-d', '--data', action='store_true', help='send command data to carbon, must be integer or '
                        'float')
+    parser.add_argument('-s', '--server', required=True, help='carbon server address')
+    parser.add_argument('-p', '--port', default=2003, help='carbon server port, default 2003')
     parser.epilog = 'default OID will be formed hostname.stats.command.[time|data], where hostname is taken from system\
                     hostname stats is static string, command is the command name passed with -c flag and time or data \
                     depending on the execution mode -t or -d'
@@ -37,24 +40,51 @@ def create_oid(hostname, command, mode):
 
 def measure_command_time(command):
     start = time()
-    commands.getoutput(command)
+    commands.getstatus(command)
     end = time()
     return end-start
 
 
-def main():
-    args = collect_arguments()
-    command = args.command
+def get_command_output(command):
+    data = commands.getoutput(command)
+    return data
+
+
+def send_data(oid, data, timestamp, verbose):
+    if verbose:
+        print "{0} {1} {2}".format(oid, data, timestamp)
+    else:
+        print "sending {0} {1} {2}".format(oid, data, timestamp)
+
+
+def get_data(args):
+    if args.time is True:
+        data = str(measure_command_time(args.command))
+    elif args.data is True:
+        data = str(get_command_output(args.command))
+    else:
+        raise 'Something really bad happend, could not get command data'
+    return data
+
+
+def get_mode(args):
     if args.time is True:
         mode = 'time'
     elif args.data is True:
         mode = 'data'
     else:
-        raise 'Something really bad happend'
+        raise 'Something really bad happend, could not get command mode'
+    return mode
+
+
+def main():
+    args = collect_arguments()
+    command = args.command
+    mode = get_mode(args)
     oid = create_oid(hostname, command, mode)
-    print oid
-#    print oid
-#    execution_time = measure_command_time(command)
+    data = get_data(args)
+    timestamp = int(time())
+    send_data(oid, data, timestamp, args.verbose)
 
 
 if __name__ == "__main__": main()  # noqa allow 2 satements on the same line
