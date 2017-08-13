f0d7f2c76651a66e04d4063cfab9aec815bb95d8
Veaceslav Mindru <mindruv@gmail.com>
2017-08-13 12:38:18 +0200

    WIP

--- check.py
+++ check.py
@@ -4,7 +4,6 @@ import commands
 from argparse import ArgumentParser
 from time import time
 import platform
-# import socket
 
 
 VERSION = "0.1"
@@ -15,27 +14,23 @@ def collect_arguments():
     parser = ArgumentParser()
     parser.prog = "collector"
     parser.description = 'collector: collects and sends stats to carbon'
-    group = parser.add_mutually_exclusive_group(required=True)
-    parser.add_argument('-v', '--version', action='version', version=VERSION, help='display version information')
-    parser.add_argument('-o', '--oid', help='OID where to store data')
-    parser.add_argument('-c', '--command', required=True, help='command to execute', metavar='')
+
+    parser.add_argument('-m', '--metric', required=True, help='OID where to store data')
+    parser.add_argument('-c', '--command', required=True, help='command to execute')
     parser.add_argument('-V', '--verbose', action='store_true', help='print data to stdout before sending to server')
-    group.add_argument('-t', '--time', action='store_true', help='send command time execution as metric to carbon')
-    group.add_argument('-d', '--data', action='store_true', help='send command data to carbon, must be integer or '
-                       'float')
     parser.add_argument('-s', '--server', required=True, help='carbon server address')
     parser.add_argument('-p', '--port', default=2003, help='carbon server port, default 2003')
+    parser.add_argument('-D', '--daemon', default=5, type=int, nargs='?', help='run as daemon with default interval of '
+                        '5 seconds, specifing a value will make the dameon run with specified interval')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-t', '--time', action='store_true', help='send command time execution as metric to carbon')
+    group.add_argument('-d', '--data', action='store_true', help='send command data to carbon, must be integer, float')
     parser.epilog = 'default OID will be formed hostname.stats.command.[time|data], where hostname is taken from system\
                     hostname stats is static string, command is the command name passed with -c flag and time or data \
                     depending on the execution mode -t or -d'
     args = parser.parse_args()
     return args
-
-
-def create_oid(hostname, command, mode):
-    oid_separator = '.'
-    oid = oid_separator.join((hostname, "stats", command, mode))
-    return oid
 
 
 def measure_command_time(command):
@@ -50,11 +45,11 @@ def get_command_output(command):
     return data
 
 
-def send_data(oid, data, timestamp, verbose):
+def send_data(metric, data, server, port, verbose):
     if verbose:
-        print "{0} {1} {2}".format(oid, data, timestamp)
+        print "{0} {1} {2}".format(metric, data)
     else:
-        print "sending {0} {1} {2}".format(oid, data, timestamp)
+        print "sending {0} {1} {2}".format(metric, data)
 
 
 def get_data(args):
@@ -79,12 +74,9 @@ def get_mode(args):
 
 def main():
     args = collect_arguments()
-    command = args.command
-    mode = get_mode(args)
-    oid = create_oid(hostname, command, mode)
     data = get_data(args)
-    timestamp = int(time())
-    send_data(oid, data, timestamp, args.verbose)
+    send_data(args.metric, data, args.server, args.port, args.verbose)
+
 
 
 if __name__ == "__main__": main()  # noqa allow 2 satements on the same line
